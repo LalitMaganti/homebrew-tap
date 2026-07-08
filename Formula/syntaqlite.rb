5a57ed9ae373030759359cb003c871575a777e66
Lalit Maganti <lalitmaganti@gmail.com>
2026-07-08 09:52:02 +0000

    Update syntaqlite to 0.7.1

--- Formula/syntaqlite.rb
+++ Formula/syntaqlite.rb
@@ -1,26 +1,26 @@
 class Syntaqlite < Formula
   desc "Fast, accurate SQLite SQL formatter, validator, and language server"
   homepage "https://syntaqlite.com"
-  version "0.7.0"
+  version "0.7.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.0/syntaqlite-macos-arm64.tar.gz"
-      sha256 "eb94d94131a69396702885cb540078096f1b8587d0318d306ff675e4c703236b"
+      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.1/syntaqlite-macos-arm64.tar.gz"
+      sha256 "d55cd20ee94c3fec9a79f9369a5b409ace3bc463c9e1ea2fd3d7f919706698ce"
     else
-      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.0/syntaqlite-macos-x64.tar.gz"
-      sha256 "3225589d68e165fdc0bc7713f723444897c3222eae961a86841c7d7138cc3ca4"
+      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.1/syntaqlite-macos-x64.tar.gz"
+      sha256 "c4ee5ce813985278335b42d92aba5956abe8b7563212706e746b17f72e2490c5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.0/syntaqlite-linux-arm64.tar.gz"
-      sha256 "99cde5e546d0c1713017b0ad994a871553253148605b7e59190bb9ba4c1d2e0f"
+      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.1/syntaqlite-linux-arm64.tar.gz"
+      sha256 "a8bb7a2de8760c496d03d04e896edbbe3caf7ac3e65aa71d0fad7983dec13409"
     else
-      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.0/syntaqlite-linux-x64.tar.gz"
-      sha256 "af06850aa30393eb4cab79c4c5ed2f1459acad5cffa706f3312567a80c7a1f39"
+      url "https://github.com/LalitMaganti/syntaqlite/releases/download/v0.7.1/syntaqlite-linux-x64.tar.gz"
+      sha256 "fb5d23bb64d8a29c0dbda4fbbbfb3176cfd812107863438b4d09816952962ed1"
     end
   end
 
